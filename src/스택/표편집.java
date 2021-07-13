7fd5c8c59b6f22760fd56b1c91ca2f6cdcba5c49
ys3135 <ys3135@gmail.com>
2021-07-13 13:11:32 +0900

    2021 카카오 채용연계형 인턴 표편집(스택,구현)

--- src/스택/표편집.java
+++ src/스택/표편집.java
@@ -1,3 +1,5 @@
+package 스택;
+
 import javax.swing.plaf.TreeUI;
 import java.awt.event.KeyListener;
 import java.io.BufferedReader;
@@ -9,22 +11,18 @@ public class 표편집 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        solution(8,2, new String[] {"D 2","C","U 3","C","D 4","C","U 2","Z","Z"});
+        solution(8,2, new String[] {"D 2","C","U 3","C","D 4","C","U 2","Z","Z","U 1","C"});
 
     }
 
     public static String solution(int n, int k, String[] cmd) {
         String answer = "";
-        ArrayList<String> list = new ArrayList<>();
-        ArrayList<String> subList = new ArrayList<>();
-        for(int i = 0 ;i < n ;i++){
-            list.add("O");
-        }
+
 
 
         Stack<Integer> removeList = new Stack<>();
+        int size = n;
 
-        int last = n-1;
 
         for(int i = 0 ; i < cmd.length;i++){
             StringTokenizer st = new StringTokenizer(cmd[i]);
@@ -35,74 +33,56 @@ public class 표편집 {
 //                "D X": 현재 선택된 행에서 X칸 아래에 있는 행을 선택합니다.
                 //아래로 가는거는 ++;
                 int plus = Integer.parseInt(st.nextToken());
-                int chk = 0;
 
+                k+= plus;
 
 
             }else if (order.equals("C")){
 //                "C" : 현재 선택된 행을 삭제한 후, 바로 아래 행을 선택합니다. 단, 삭제된 행이 가장 마지막 행인 경우 바로 윗 행을 선택합니다.
-                list.set(k,"X");
-                removeList.add(k);
-                if(k == last){
-                    for(int j = last ; j >= 0 ; j--){
-                        if(list.get(j).equals("O")){
-                            last = j;
-                            k = j;
-                            break;
-                        }
-                    }
-
-                }else{
-                    for(int j = k ; j < n ;j++){
-                        if(list.get(j).equals("O")){
-                            k = j;
-                            break;
-                        }
-                    }
-
+                removeList.push(k);
+                size -= 1;
+                if(k == size){
+                    k -=1 ;
                 }
+
 
             }else if (order.equals("U")){
                 //"U X": 현재 선택된 행에서 X칸 위에 있는 행을 선택합니다.
                 //위로 가는거는 --
                 int minus = Integer.parseInt(st.nextToken());
 
-                int chk = 0;
-                while(chk <= minus-1){
-                    if(list.get(k-1).equals("O")){
-                        chk++;
-                    }
-                    k -=1;
-                }
+                k -= minus;
+
 
             }else if (order.equals("Z")){
 //                "Z" : 가장 최근에 삭제된 행을 원래대로 복구합니다. 단, 현재 선택된 행은 바뀌지 않습니다.
-                    int recover = removeList.pop();
-                    list.set(recover,"O");
-                    if(recover > last){
-                        last = recover;
-                    }
+                size+=1;
+                int recover = removeList.pop();
+                if(recover <= k){
+                    k+=1;
+                }
             }
 
             System.out.println("명령"+order);
             System.out.println("인덱스"+k);
-            System.out.println(list);
+            System.out.println("사이즈"+size);
+
 
 
         }
         System.out.println(removeList);
 
+        StringBuilder sb=  new StringBuilder();
 
-
-        System.out.println(list);
-
-        String tmp = list.toString().replaceAll("[\\[\\], ]","");
-
-        StringBuilder sb = new StringBuilder();
-
-        for(String word : list){
-            sb.append(word);
+        for(int i = 0 ; i < size;i++){
+            sb.append("O");
         }
+
+        while(!removeList.isEmpty()){
+            sb.insert(removeList.pop(),"X");
+        }
+        System.out.println(sb.toString());
+
 
         answer = sb.toString();
 
