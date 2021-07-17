547ec12657cbd48553ec6ce5b9f57843b7b45b75
ys3135 <ys3135@gmail.com>
2021-07-17 19:16:47 +0900

    백준 12015 가장 긴 증가하는 부분수열2 (이분탐색)

---
+++ src/이분탐색/가장긴증가하는부분수열2.java
@@ -0,0 +1,65 @@
+package 이분탐색;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class 가장긴증가하는부분수열2 {
+
+    static ArrayList<Integer> list;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n =  Integer.parseInt(br.readLine());
+
+        StringTokenizer st=  new StringTokenizer(br.readLine());
+        list = new ArrayList<>();
+        list.add(0);
+
+        for(int i = 0 ; i < n;i++){
+            int input = Integer.parseInt(st.nextToken());
+
+            if(list.get(list.size()-1) < input){
+                list.add(input);
+            }else{
+                System.out.println(input);
+                // 더 작다 그럼 증가하는것이 아니니깐
+                list.set(BS(input),input);
+            }
+        }
+
+
+        System.out.println(list.size()-1);
+
+
+    }
+
+    public static int BS(int input){
+        int left = 0;
+        int right = list.size()-1;
+
+        while(left <right){
+            int mid = (left+right)/2;
+            if (list.get(mid) < input) {
+                left = mid+1;
+            }else {
+                right = mid;
+            }
+
+
+        }
+
+
+
+        return (left+right)/2;
+    }
+}
+
+
+
+
+
+
