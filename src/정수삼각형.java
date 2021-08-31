7bcc250ea429c87a994d5462bb098eacadb28e3c
ys3135 <ys3135@gmail.com>
2021-08-31 23:37:04 +0900

    프로그래머스 정수삼각형

---
+++ src/정수삼각형.java
@@ -0,0 +1,22 @@
+public class 정수삼각형 {
+
+    public static void main(String[] args) {
+
+    }
+
+
+    public static int solution(int[][] triangle) {
+        int answer = 0;
+
+
+        for(int i = triangle.length-1 ; i >0 ;i--) {
+            for(int j = 0 ;j < triangle[i].length-1;j++) {
+                triangle[i-1][j] += Math.max(triangle[i][j], triangle[i][j+1]);
+            }
+        }
+
+        answer = triangle[0][0];
+
+        return answer;
+    }
+}
