c651a39bc218b9741b2f14375dd54719174367b4
yssy3135 <ys3135@gmail.com>
2023-12-08 23:05:37 +0900

    [ADD] 프로그래머스 연속된 부분 수열의 합

---
+++ src/구현/programmers_연속된_부분_수열의_합.java
@@ -0,0 +1,54 @@
+package 구현;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class programmers_연속된_부분_수열의_합 {
+    public int[] solution(int[] sequence, int k) {
+        int[] answer = sequence;
+
+
+
+        int left = 0;
+        int right = 0;
+
+        int minLeft = 0;
+        int minRight = sequence.length;
+
+        int sum = sequence[0];
+
+        while(left <= right ) {
+
+            if(sum == k ) {
+                if(right - left < minRight - minLeft) {
+                    minRight = right;
+                    minLeft = left;
+                }
+
+                sum -= sequence[left];
+                left++;
+                if(left >= sequence.length) break;
+
+            }
+
+            if(sum < k) {
+                right++;
+
+                if(right >= sequence.length) break;
+                sum += sequence[right];
+            }
+
+            if(sum > k) {
+                sum -= sequence[left];
+                left++;
+            }
+
+
+
+
+        }
+
+        return new int[] {minLeft, minRight};
+    }
+}
